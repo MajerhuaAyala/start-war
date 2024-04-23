7b70df11f29667b22ff14b987b2a9ea5a67a8c0c
MarcelinoMajerhua <marcelino.majerhua@gmail.com>
2024-04-23 07:55:04 -0500

    fix: change name of entity vehicle and film

--- src/context/film/infraestructure/persistence/typeorm/film.entity.ts
+++ src/context/film/infraestructure/persistence/typeorm/film.entity.ts
@@ -1,6 +1,6 @@
 import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
 
-@Entity("people")
+@Entity("films")
 export class FilmEntity {
     @PrimaryGeneratedColumn("uuid")
     id: string;
--- src/context/vehicle/infraestructure/persistence/typeorm/vehicle.entity.ts
+++ src/context/vehicle/infraestructure/persistence/typeorm/vehicle.entity.ts
@@ -1,6 +1,6 @@
 import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
 
-@Entity("people")
+@Entity("vehicles")
 export class VehicleEntity {
     @PrimaryGeneratedColumn("uuid")
     id: string;
