e6643e5f46dd5d2d4e7d71f0c80f9a0073aa2064
MarcelinoMajerhua <marcelino.majerhua@gmail.com>
2024-04-24 18:22:11 -0500

    feat: add test when create new film and feild

--- src/context/film/domain/FilmTitle.ts
+++ src/context/film/domain/FilmTitle.ts
@@ -1,10 +1,21 @@
-import { StringValueObject } from "../../../shared/domain/valueObject/StringValueObject";
+import {StringValueObject} from "../../../shared/domain/valueObject/StringValueObject";
+import {InvalidArgumentError} from "../../../shared/domain/valueObject/InvalidArgumentError";
 
 export class FilmTitle extends StringValueObject {
   readonly value: string;
+  readonly formatTitle = /^[a-zA-Z0-9\- ]+$/;
 
   constructor(value: string) {
     super(value, true, "titulo");
     this.value = value;
+    this.ensureFormatTitle(value)
   }
+
+  private ensureFormatTitle(input: string) {
+    const response = this.formatTitle.test(input)
+    if (!response) {
+      throw new InvalidArgumentError(`Formato del campo ${this.nameAttribute} es invalida`)
+    }
+  }
+
 }
--- test/context/film/application/FilmCreator.test.ts
+++ test/context/film/application/FilmCreator.test.ts
@@ -1,6 +1,7 @@
 import {FilmRepositoryMock} from "../__mocks__/FilmRepositoryMock";
 import {FilmCreator} from "../../../../src/context/film/application/create/FilmCreator";
 import {FilmMother} from "../domain/FilmMother";
+import {BadRequestError} from "../../../../src/shared/domain/type-error";
 
 let filmRepository: FilmRepositoryMock
 let filmCreator: FilmCreator
@@ -29,6 +30,38 @@ describe("Crear nueva pelicula", () => {
         filmRepository.assertSaveHaveBeenCalledWith(newFilmRandom)
 
         expect(response.getRight()).toMatchObject(newFilmRandom)
+      })
+    })
+    describe("La película ya está registrada", () => {
+      it("Debería devolver un error con el mensaje de La película con el id: 12345 ya está registrada.", async () => {
+        const newFilmRandom = FilmMother.random() // se crea un film con atributos random
+
+        filmRepository.returnOnFindById(newFilmRandom) // le doy la intrucción de que el método findById devuelva un film idéntico al newFilmRandom
+
+        const response = await filmCreator.run({
+          id: newFilmRandom.id.value,
+          apertura: newFilmRandom.apertura.value,
+          director: newFilmRandom.director.value,
+          episodio: newFilmRandom.episodio.value,
+          titulo: newFilmRandom.titulo.value
+        })
+
+        expect(response.getLeft()).toMatchObject(new BadRequestError(`La película con el id: ${newFilmRandom.id.value} ya está registrada.`))
+
+      })
+    })
+
+    describe("El titulo de la pelula está es vacia", () => {
+      it("Debería devolve una excepcion con el mensaje de Formato del campo titulo es invalida", async () => {
+        const newFilmRandom = FilmMother.random() // se crea un film con atributos random
+        filmRepository.returnOnFindByIdEmpty() // el medoto de findById del repositorio devolverá un Optional.empty() que significa que no hay un fil registrado con el mismo id
+        await expect(filmCreator.run({
+          id: newFilmRandom.id.value,
+          apertura: newFilmRandom.apertura.value,
+          director: newFilmRandom.director.value,
+          episodio: newFilmRandom.episodio.value,
+          titulo: `${newFilmRandom.titulo.value}-**` // lo valores ** no cumplen con el formato de titulo
+        })).rejects.toThrow("Formato del campo titulo es invalida")
       })
     })
   })
